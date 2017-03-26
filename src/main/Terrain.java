9f9370e06dc442f01d40470d095a3adfe710c1ac
Joshua Novick <jnovick@ufl.edu>
2017-03-26 15:22:10 -0400

    Add Terrain with tests

---
+++ src/main/Terrain.java
@@ -0,0 +1,20 @@
+public enum Terrain
+{
+    Sea(true),
+    Pasture(false),
+    Woods(false),
+    Rock(false),
+    Desert(false),
+    Mountain(false);
+
+    private final boolean canConnectRiver;
+
+    Terrain(boolean canConnectRiver)
+    {
+        this.canConnectRiver=canConnectRiver;
+    }
+
+    public boolean canConnectRiver() {
+        return canConnectRiver;
+    }
+}
