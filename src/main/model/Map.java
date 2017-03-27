d3b8efac8c3815642ad88da9d9ec95a42f7e2799
Austin Lindenberg <austindlindenber@ufl.edu>
2017-03-27 18:21:00 -0400

    Added map, Tonny did tile

--- src/main/model/Map.java
+++ src/main/model/Map.java
@@ -1,10 +1,26 @@
 package model;
 
-/**
- * Created by Konrad on 3/26/2017.
- */
-public class Map {
+import model.tile.Location;
+import model.tile.Terrain;
+import model.tile.Tile;
 
-    public Map(){}
+import java.util.HashMap;
+
+public class Map {
+    private java.util.Map<Location, Tile> tiles;
+    public Map(){
+        tiles = new HashMap<Location, Tile>();
+        this.initialize();
+    }
+
+//    Completely ignore this, just for Anip to work with a small initial map on FileExporter.
+    private void initialize() {
+        tiles.put(new Location(0,0,0), new Tile(Terrain.SEA));
+        tiles.put(new Location(0,-1,1), new Tile(Terrain.SEA));
+        tiles.put(new Location(0,1,-1), new Tile(Terrain.SEA));
+        tiles.put(new Location(1,0,-1), new Tile(Terrain.SEA));
+        tiles.put(new Location(-1,0,1), new Tile(Terrain.SEA));
+        tiles.put(new Location(1,-1,0), new Tile(Terrain.SEA));
+    }
 
 }
