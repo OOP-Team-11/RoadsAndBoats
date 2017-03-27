0451c1e948dcad8a6e54cb550c284f5f2b018c89
konradpabjan <konradpabjan@outlook.com>
2017-03-27 19:38:30 -0400

    General Work on views and mapMakerController
    Lots of stuff

--- src/main/view/MapMakerView.java
+++ src/main/view/MapMakerView.java
@@ -1,6 +1,12 @@
 package view;
 
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+
+import static java.awt.event.PaintEvent.PAINT;
 
 /**
  * Created by Konrad on 3/26/2017.
@@ -8,12 +14,49 @@ import javafx.scene.canvas.Canvas;
 public class MapMakerView {
 
     private Canvas canvas;
+    private GraphicsContext gc;
+    private int cameraX;
+    private int cameraY;
+    private int cameraZoom;
+
     public MapMakerView(Canvas canvas){
+
         this.canvas = canvas;
+        this.gc = canvas.getGraphicsContext2D();
     }
 
     // public method called by GameLoop when refresh is necessary
     public void render(){
-        System.out.println("MapMakerView is rendering");
+        drawDivider();
+    }
+
+    // camera offset function to pan/scroll placement area
+    public void moveCamera(int moveX, int moveY){
+        this.cameraX += moveX;
+        this.cameraY += moveY;
+    }
+
+    // camera zoom function to move in/out of placement area
+    public void changeZoom(int moveZ){
+        this.cameraZoom += moveZ;
+    }
+    private void setGraphicsContentStroke(Paint p){
+        this.gc.setStroke(p);
+    }
+    private void setGraphicsContentFill(Paint p){
+        this.gc.setFill(p);
+    }
+    private void setLineWidth(double width){
+        this.gc.setLineWidth(width);
+    }
+    private void drawLine(double x1, double y1, double x2, double y2){
+        this.gc.strokeLine(x1,y1,x2,y2);
+    }
+
+    private void drawDivider(){
+        setGraphicsContentStroke(Color.BLACK);
+        setGraphicsContentFill(Color.BLACK);
+        setLineWidth(5.0);
+        drawLine(0,0,canvas.getWidth(),0);
     }
 }
