39e06ca3dea4c747c54d3de5fb988a946d27213e
Juan Jauregui <juan@jauregui.co>
2017-03-27 18:31:54 -0400

    Figured out how to handle notifyObservers

--- src/main/controller/ControlHandler.java
+++ src/main/controller/ControlHandler.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import model.Map;
 import model.tile.Location;
@@ -16,8 +17,9 @@ import view.TileSelectorView;
 
 
 class ControlHandler implements CursorObserverSubject, TileSelectObserverSubject {
-    private ArrayList<CursorObserver> cursorObservers;
-    private ArrayList<TileSelectObserver> tileSelectObservers;
+    private ArrayList<CursorObserver> cursorObservers;          //Hold CursorObservers who've registered for Cursor updates
+    private ArrayList<TileSelectObserver> tileSelectObservers;  //Hold TileSelectObservers who've registered for TileSelect updates
+    HashMap<ArrayList,Boolean> observerUpdateFlags;  //Will flag a need to update one or both sets of observers when notifyObservers() is called
 
     private Map gameMap;
     private Tile protoTile;
@@ -26,6 +28,10 @@ class ControlHandler implements CursorObserverSubject, TileSelectObserverSubject
     // mapMakerView is given as an observer that the map will use to notify
     // tileSelectorView is given as an observer that ControlHandler will notify
     public ControlHandler(Map gameMap, MapMakerView mapMakerView, TileSelectorView tileSelectorView){
+        cursorObservers = new ArrayList<>();
+        tileSelectObservers = new ArrayList<>();
+        observerUpdateFlags = new HashMap<>();
+
         this.gameMap = gameMap;
         registerCursorObserver(mapMakerView);
         registerTileSelectObserver(tileSelectorView);
@@ -33,7 +39,25 @@ class ControlHandler implements CursorObserverSubject, TileSelectObserverSubject
 
      @Override
      public void notifyObservers() {
+        if(observerUpdateFlags.get(cursorObservers)){
+            notifyCursorObservers();
+        }
 
+        if(observerUpdateFlags.get(tileSelectObservers)){
+            notifyTileSelectObservers();
+        }
+     }
+
+     public void notifyCursorObservers(){
+        for(int i = 0; i < cursorObservers.size(); i++){
+            //Notify cursorObservers.get(i) of changes
+        }
+     }
+
+     public void notifyTileSelectObservers(){
+         for(int i = 0; i < tileSelectObservers.size(); i++){
+             //Notify tileSelectObservers.get(i) of changes
+         }
      }
 
      @Override
