69c92c99fbcb6e4ef09737c22c2f383f6b8a7533
Austin Lindenberg <austindlindenber@ufl.edu>
2017-03-29 00:34:02 -0400

    Moving viewport

--- src/main/view/render/MapMakerCursorInfo.java
+++ src/main/view/render/MapMakerCursorInfo.java
@@ -5,10 +5,14 @@ import model.tile.Location;
 public class MapMakerCursorInfo {
     private Location cursorLocation;
     private boolean isCursorValid;
+    private int cameraX;
+    private int cameraY;
 
     public MapMakerCursorInfo(Location cursorLocation, boolean isValid ) {
         this.cursorLocation = cursorLocation;
         this.isCursorValid = isValid;
+        this.cameraX = 0;
+        this.cameraY = 0;
     }
 
     public Location getCursorLocation() {
@@ -22,5 +26,17 @@ public class MapMakerCursorInfo {
     }
     public void setIsCursorValid(boolean isValid) {
         this.isCursorValid = isValid;
+    }
+    public void setCameraX(int camX) {
+        this.cameraX = camX;
+    }
+    public int getCameraX() {
+        return this.cameraX;
+    }
+    public void setCameraY(int camY) {
+        this.cameraY = camY;
+    }
+    public int getCameraY() {
+        return this.cameraY;
     }
 }
