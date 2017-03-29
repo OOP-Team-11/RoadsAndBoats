645659f2b55baa7cc2e48ce7897d80fff6f0f630
Austin Lindenberg <austindlindenber@ufl.edu>
2017-03-28 20:57:55 -0400

    Information objects for mapMaker and tileSelector

---
+++ src/main/view/render/MapMakerCursorInfo.java
@@ -0,0 +1,21 @@
+package view.render;
+
+import model.tile.Location;
+
+public class MapMakerCursorInfo {
+    private Location cursorLocation;
+    private boolean isCursorValid;
+
+    public Location getCursorLocation() {
+        return cursorLocation;
+    }
+    public void setCursorLocation(Location cursorLoc) {
+        this.cursorLocation = cursorLocation;
+    }
+    public boolean getIsCursorValid() {
+        return isCursorValid;
+    }
+    public void setIsCursorValid(boolean isValid) {
+        this.isCursorValid = isValid;
+    }
+}
---
+++ src/main/view/render/MapMakerRenderInfo.java
@@ -0,0 +1,15 @@
+package view.render;
+
+import model.tile.Tile;
+import java.util.Collection;
+
+public class MapMakerRenderInfo {
+    private Collection<Tile> tileInformation;
+
+    public Collection<Tile> getTileInformation() {
+        return this.tileInformation;
+    }
+    public void setTileInformation(Collection<Tile> tileInfo) {
+        this.tileInformation = tileInfo;
+    }
+}
