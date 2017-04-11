88c590944c35ca7cc9b5952bad827abd72b2d878
Gavin Scheele <gdscheele@gmail.com>
2017-04-11 18:33:09 -0400

    Gavin - removed running tests for mapmaker

---
+++ src/test/game/ExampleTest.java
@@ -0,0 +1,13 @@
+package game;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void example() {
+        assertTrue(true);
+    }
+}
