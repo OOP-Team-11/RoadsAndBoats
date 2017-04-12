7ac1d0902866688f55500322a587c483126fa531
Tonny <tonnyx1294@hotmail.com>
2017-04-11 20:45:19 -0400

    added Wonder subsystem with TurnObserver interface

---
+++ src/main/game/model/wonder/WonderBrick.java
@@ -0,0 +1,15 @@
+package game.model.wonder;
+
+public abstract class WonderBrick {
+
+//    private boolean isNeutral;
+
+    WonderBrick() {
+
+    }
+
+//    public boolean isNeutral() {
+//        return isNeutral;
+//    }
+
+}
