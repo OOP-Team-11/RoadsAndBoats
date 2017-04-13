76e84aa1ae7569b4d336bf7ee361c9125acb9354
Austin Lindenberg <austindlindenber@ufl.edu>
2017-04-13 18:33:51 -0400

    Fixed abilities for manager and added tileCompartmentLocation

--- src/main/game/model/direction/TileCompartmentLocation.java
+++ src/main/game/model/direction/TileCompartmentLocation.java
@@ -1,15 +1,32 @@
 package game.model.direction;
 
+import static java.util.Objects.hash;
+
 public class TileCompartmentLocation {
     private Location location;
-    private TileCompartmentDirection tcd;
+    private TileCompartmentDirection tileCompartmentDirection;
 
     public TileCompartmentLocation(Location loc, TileCompartmentDirection tcd) {
         this.location = loc;
-        this.tcd = tcd;
+        this.tileCompartmentDirection = tcd;
     }
 
     public Location getLocation() {
         return location;
+    }
+    public TileCompartmentDirection getTileCompartmentDirection() {
+        return tileCompartmentDirection;
+    }
+
+    @Override
+    public int hashCode() {
+        return hash(this.location);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        return (object instanceof TileCompartmentLocation) &&
+                ((TileCompartmentLocation) object).getLocation() == this.location &&
+                ((TileCompartmentLocation) object).getTileCompartmentDirection() == this.getTileCompartmentDirection();
     }
 }
