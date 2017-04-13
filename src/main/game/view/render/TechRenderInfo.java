51873d74759aff7d78a160663fd0bc0826c7877f
konradpabjan <konradpabjan@outlook.com>
2017-04-13 02:43:23 -0400

    Research View sexiness

--- src/main/game/view/render/TechRenderInfo.java
+++ src/main/game/view/render/TechRenderInfo.java
@@ -1,4 +1,14 @@
 package game.view.render;
 
 public class TechRenderInfo {
+
+
+    // TODO need 7 booleans for each technology
+    // TODO need List of Transporters that can be used for research
+    // TODO need Phase
+
+    public TechRenderInfo(){
+
+    }
+
 }
