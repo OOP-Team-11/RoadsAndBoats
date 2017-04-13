f0eff75a13b7a51dde298bd11e362d376f17ce9c
konradpabjan <konradpabjan@outlook.com>
2017-04-13 13:42:28 -0400

    Improvements for wonder view and controller

--- src/main/game/controller/WonderViewController.java
+++ src/main/game/controller/WonderViewController.java
@@ -1,12 +1,30 @@
 package game.controller;
 
 import game.view.WonderView;
+import javafx.event.EventHandler;
+import javafx.scene.input.MouseEvent;
 
 public class WonderViewController {
 
     private WonderView wonderView;
 
     public WonderViewController(WonderView wonderView){
+        setView(wonderView);
+        initializeEvents();
+    }
+
+    private void setView(WonderView wonderView){
         this.wonderView = wonderView;
     }
+
+    private void initializeEvents(){
+        EventHandler<MouseEvent> eventHandler = new EventHandler<MouseEvent>() {
+            public void handle(MouseEvent e) {
+                // TODO hook up to model to update when a brick was added
+                System.out.println("Brick was added to wonder");
+            }
+        };
+        this.wonderView.addEventFilterToAddBrickButton(eventHandler);
+    }
+
 }
