17a6502c079a162361e00b4b01a5e069a50691f7
Joshua Novick <jnovick@ufl.edu>
2017-04-14 11:12:49 -0400

    View render info objects created

--- src/main/game/view/render/StructureRenderInfo.java
+++ src/main/game/view/render/StructureRenderInfo.java
@@ -1,4 +1,17 @@
 package game.view.render;
 
-public class StructureRenderInfo {
+import game.model.direction.TileCompartmentLocation;
+import game.model.structures.Structure;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class StructureRenderInfo
+{
+    public final HashMap<TileCompartmentLocation, List<Structure>> structures;
+
+    public StructureRenderInfo(HashMap<TileCompartmentLocation, List<Structure>> structures)
+    {
+        this.structures = structures;
+    }
 }
--- src/main/game/view/render/TransportRenderInfo.java
+++ src/main/game/view/render/TransportRenderInfo.java
@@ -1,4 +1,17 @@
 package game.view.render;
 
-public class TransportRenderInfo {
+import game.model.direction.TileCompartmentLocation;
+import game.model.transport.Transport;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class TransportRenderInfo
+{
+    public final HashMap<TileCompartmentLocation, List<Transport>> transports;
+
+    public TransportRenderInfo(HashMap<TileCompartmentLocation, List<Transport>> transports)
+    {
+        this.transports = transports;
+    }
 }
