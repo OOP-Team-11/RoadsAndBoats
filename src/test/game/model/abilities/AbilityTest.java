33e7267364eafd99f9b32cf1e66f38f07c517183
Gavin Scheele <gdscheele@gmail.com>
2017-04-15 13:12:26 -0400

    Gavin - transport capacity refactor

--- src/test/game/model/abilities/AbilityTest.java
+++ src/test/game/model/abilities/AbilityTest.java
@@ -29,7 +29,7 @@ public class AbilityTest {
         GooseManager gooseManager = new GooseManager();
         MainViewController mainViewController = new MainViewController();
         Player player1 = new Player(new TransportAbilityManager(mainViewController, gooseManager), new PlayerId(2), "gavin");
-        Transport donkey = new DonkeyTransport(player1.getPlayerId(), new TransportId(), 2,2);
+        Transport donkey = new DonkeyTransport(player1.getPlayerId(), new TransportId());
         RBMap map = new RBMap();
         map.placeTile(new Location(0,0,0), new Tile(Terrain.PASTURE, RiverConfiguration.getNoRivers()));
 //        Add 2 geese to same location that donkey is at so he will have 2 followAbilities. One or both follow.
