5048ba7aad52b8da296d367375fcef710a0c73e8
Joshua Novick <jnovick@ufl.edu>
2017-04-17 05:16:18 -0400

    Attach research observer

--- src/main/game/GameInitializer.java
+++ src/main/game/GameInitializer.java
@@ -48,6 +48,7 @@ public class GameInitializer {
         PlayerId player1Id = new PlayerId(1);
         TileCompartmentLocation player1StartingLocation = new TileCompartmentLocation(new Location(0,0,0), TileCompartmentDirection.getNorth());
         ResearchManager p1ResearchManager = new ResearchManager(player1StartingLocation.getLocation(), player1Id);
+        p1ResearchManager.attach(viewHandler.getResearchViewReference());
         TransportManager player1TransportManager = new TransportManager(player1Id, controllerManager.getMainViewController(), gooseManager, map, structureManager, p1ResearchManager);
         Player player1 = new Player(player1TransportManager, new PlayerId(1), player1Name, player1StartingLocation);
         player1.attach(viewHandler.getMainViewReference());
@@ -56,6 +57,7 @@ public class GameInitializer {
         PlayerId player2Id = new PlayerId(2);
         TileCompartmentLocation player2StartingLocation = new TileCompartmentLocation(new Location(0,1,-1), TileCompartmentDirection.getNorth());
         ResearchManager p2ResearchManager = new ResearchManager(player2StartingLocation.getLocation(), player2Id);
+        p2ResearchManager.attach(viewHandler.getResearchViewReference());
         TransportManager player2TransportManager = new TransportManager(player2Id, controllerManager.getMainViewController(), gooseManager, map, structureManager, p2ResearchManager);
         Player player2 = new Player(player2TransportManager, new PlayerId(2), player2Name, player2StartingLocation);
         player2.attach(viewHandler.getMainViewReference());
