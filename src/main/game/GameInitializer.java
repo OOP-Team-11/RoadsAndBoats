bce59d773eaf486713ab6dce52b670a26fefc362
Gavin Scheele <gdscheele@gmail.com>
2017-04-17 00:39:05 -0400

    Gavin - fixed map file formats. Added error message for invalid map.

--- src/main/game/GameInitializer.java
+++ src/main/game/GameInitializer.java
@@ -25,62 +25,58 @@ public class GameInitializer {
     private ViewHandler viewHandler;
     private ControllerManager controllerManager;
 
-    public GameInitializer(String gameFile, String player1Name, String player2Name, Stage primaryStage){
+    public GameInitializer(String gameFile, String player1Name, String player2Name, Stage primaryStage) throws MalformedMapFileException, IOException {
 
-        System.out.println("New Game has started");        viewHandler = new ViewHandler(primaryStage);
+        System.out.println("New Game has started");
+        viewHandler = new ViewHandler(primaryStage);
         controllerManager = new ControllerManager(viewHandler);
 
         GameExporter gameExporter;
-        try {
-            RBMap map = new RBMap();
-            map.attach(viewHandler.getMainViewReference());
-            map.attachMapResourceRenderInfoObserver(viewHandler.getMainViewReference());
+        RBMap map = new RBMap();
+        map.attach(viewHandler.getMainViewReference());
+        map.attachMapResourceRenderInfoObserver(viewHandler.getMainViewReference());
 
-            GooseManager gooseManager = new GooseManager(controllerManager.getMainViewController(), map);
-            StructureManager structureManager = new StructureManager(controllerManager.getMainViewController(), map);
-            structureManager.attach(viewHandler.getMainViewReference());
+        GooseManager gooseManager = new GooseManager(controllerManager.getMainViewController(), map);
+        StructureManager structureManager = new StructureManager(controllerManager.getMainViewController(), map);
+        structureManager.attach(viewHandler.getMainViewReference());
 
-            PlayerId player1Id = new PlayerId(1);
-            TransportManager player1TransportManager = new TransportManager(player1Id, controllerManager.getMainViewController(), gooseManager, map, structureManager);
-            TileCompartmentLocation player1StartingLocation = new TileCompartmentLocation(new Location(0,0,0), TileCompartmentDirection.getNorth());
-            Player player1 = new Player(player1TransportManager, new PlayerId(1), player1Name, player1StartingLocation);
-            player1.attach(viewHandler.getMainViewReference());
-            player1.attach(viewHandler.getTransportViewReference());
+        PlayerId player1Id = new PlayerId(1);
+        TransportManager player1TransportManager = new TransportManager(player1Id, controllerManager.getMainViewController(), gooseManager, map, structureManager);
+        TileCompartmentLocation player1StartingLocation = new TileCompartmentLocation(new Location(0,0,0), TileCompartmentDirection.getNorth());
+        Player player1 = new Player(player1TransportManager, new PlayerId(1), player1Name, player1StartingLocation);
+        player1.attach(viewHandler.getMainViewReference());
+        player1.attach(viewHandler.getTransportViewReference());
 
-            PlayerId player2Id = new PlayerId(2);
-            TransportManager player2TransportManager = new TransportManager(player2Id, controllerManager.getMainViewController(), gooseManager, map, structureManager);
-            TileCompartmentLocation player2StartingLocation = new TileCompartmentLocation(new Location(0,1,-1), TileCompartmentDirection.getNorth());
-            Player player2 = new Player(player2TransportManager, new PlayerId(2), player2Name, player2StartingLocation);
-            player2.attach(viewHandler.getMainViewReference());
-            player2.attach(viewHandler.getTransportViewReference());
+        PlayerId player2Id = new PlayerId(2);
+        TransportManager player2TransportManager = new TransportManager(player2Id, controllerManager.getMainViewController(), gooseManager, map, structureManager);
+        TileCompartmentLocation player2StartingLocation = new TileCompartmentLocation(new Location(0,1,-1), TileCompartmentDirection.getNorth());
+        Player player2 = new Player(player2TransportManager, new PlayerId(2), player2Name, player2StartingLocation);
+        player2.attach(viewHandler.getMainViewReference());
+        player2.attach(viewHandler.getTransportViewReference());
 
-            gooseManager.addTransportManager(player1.getTransportManager());
-            gooseManager.addTransportManager(player2.getTransportManager());
+        gooseManager.addTransportManager(player1.getTransportManager());
+        gooseManager.addTransportManager(player2.getTransportManager());
 
 
-            importFile(gameFile, map, structureManager);
+        importFile(gameFile, map, structureManager);
 
-            Game game = new Game(map, player1, player2, gooseManager, structureManager);
+        Game game = new Game(map, player1, player2, gooseManager, structureManager);
 
-            game.attachPlayerInfoObserver(viewHandler.getMainViewReference());
-            game.attachPlayerInfoObserver(viewHandler.getTransportViewReference());
+        game.attachPlayerInfoObserver(viewHandler.getMainViewReference());
+        game.attachPlayerInfoObserver(viewHandler.getTransportViewReference());
 
-            game.attachPhaseInfoObserver(viewHandler.getMainViewReference());
+        game.attachPhaseInfoObserver(viewHandler.getMainViewReference());
 
-            game.attachPlayerInfoObserver(viewHandler.getResearchViewReference());
-            game.attachPhaseInfoObserver(viewHandler.getResearchViewReference());
-            game.attachPhaseInfoObserver(viewHandler.getMainViewReference());
-            game.attachPlayerInfoObserver(viewHandler.getWonderViewReference());
-            game.attachPhaseInfoObserver(viewHandler.getWonderViewReference());
+        game.attachPlayerInfoObserver(viewHandler.getResearchViewReference());
+        game.attachPhaseInfoObserver(viewHandler.getResearchViewReference());
+        game.attachPhaseInfoObserver(viewHandler.getMainViewReference());
+        game.attachPlayerInfoObserver(viewHandler.getWonderViewReference());
+        game.attachPhaseInfoObserver(viewHandler.getWonderViewReference());
 
-            controllerManager.getMainViewController().setGame(game);
+        controllerManager.getMainViewController().setGame(game);
 
-            //TODO: Add a controller and view element to trigger this gameExporter's exportGameToPath()
-            gameExporter = new GameExporter(game);
-        } catch (MalformedMapFileException|IOException e) {
-            System.out.println(e);
-            System.exit(1);
-        }
+        //TODO: Add a controller and view element to trigger this gameExporter's exportGameToPath()
+        gameExporter = new GameExporter(game);
 
         viewHandler.startGameLoop();
     }
--- src/main/startApplication/WelcomeView.java
+++ src/main/startApplication/WelcomeView.java
@@ -4,6 +4,7 @@ import game.view.utilities.Assets;
 import javafx.animation.FadeTransition;
 import javafx.collections.ObservableList;
 import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
@@ -224,6 +225,15 @@ public class WelcomeView {
         ft.setToValue(1);
         ft.play();
     }
+
+    public void showAlert(String title, String headerText, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
 
 
     public Button getStartGameButtonReference() { return startNewGameButoon; }
